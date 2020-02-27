6e55c5ff86d1dfa8700bbcd797ac8c49152ce8ff
KylieBaek <59535282+KylieBaek@users.noreply.github.com>
2020-02-27 14:53:10 +0900

    jeju web scraping
    jeju web scraping

--- location_web_scraping/jeju.py
+++ location_web_scraping/jeju.py
@@ -10,15 +10,16 @@ spots = list(db.jejulink.find({}))
 
 for spot in spots:
     driver.get(spot['link'])
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    image =soup.select_one('meta[property="og:image"]')['content']
-    location =driver.find_element_by_xpath('//*[@id="contents"]/div[2]/div[3]/div[2]/div/div/ul/li[3]/span').text
-    description=driver.find_element_by_xpath('//*[@id="contents"]/div[2]/div[3]/div[1]/div/div/p').text
-    if location is not None:
-        doc = {'name': spot['name'],
-               'image':image ,
-               'location': location,
-               'description':description}
-        db.jeju.insert_one(doc)
+    if spot['link'] !='https://korean.visitkorea.or.kr/detail/ms_detail.do?cotid=0fca6358-2b14-41d3-9fb0-f5d79a94f528':
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        image =soup.select_one('meta[property="og:image"]')['content']
+        location =driver.find_element_by_xpath('//*[@id="contents"]/div[2]/div[3]/div[2]/div/div/ul/li[3]/span').text
+        description=driver.find_element_by_xpath('//*[@id="contents"]/div[2]/div[3]/div[1]/div/div/p').text
+        if location is not None:
+            doc = {'name': spot['name'],
+                   'image':image ,
+                   'location': location,
+                   'description':description}
+            db.jeju.insert_one(doc)
 
 driver.quit()
